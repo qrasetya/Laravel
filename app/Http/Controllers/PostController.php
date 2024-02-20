1d4f69ab3bdbf7e5fe181ce835a462696817a98a
EkaPrasetyaWibowo <dikaeka123@gmail.com>
2024-02-20 13:56:43 +0700

    show and edit

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,6 +12,8 @@ use Illuminate\Http\Request;
 
 use Illuminate\Http\RedirectResponse;
 
+use Illuminate\Support\Facades\Storage;
+
 class PostController extends Controller
 {
     /**
@@ -75,5 +77,56 @@ class PostController extends Controller
 
         //render view with post
         return view('posts.show', compact('post'));
+    }
+
+    public function edit(string $id): View
+    {
+        //get post by ID
+        $post = Post::findOrFail($id);
+
+        //render view with post
+        return view('posts.edit', compact('post'));
+    }
+
+    public function update(Request $request, $id): RedirectResponse
+    {
+        //validate form
+        $this->validate($request, [
+            'image'     => 'image|mimes:jpeg,jpg,png|max:2048',
+            'title'     => 'required|min:5',
+            'content'   => 'required|min:10'
+        ]);
+
+        //get post by ID
+        $post = Post::findOrFail($id);
+
+        //check if image is uploaded
+        if ($request->hasFile('image')) {
+
+            //upload new image
+            $image = $request->file('image');
+            $image->storeAs('public/posts', $image->hashName());
+
+            //delete old image
+            Storage::delete('public/posts/'.$post->image);
+
+            //update post with new image
+            $post->update([
+                'image'     => $image->hashName(),
+                'title'     => $request->title,
+                'content'   => $request->content
+            ]);
+
+        } else {
+
+            //update post without image
+            $post->update([
+                'title'     => $request->title,
+                'content'   => $request->content
+            ]);
+        }
+
+        //redirect to index
+        return redirect()->route('posts.index')->with(['success' => 'Data Berhasil Diubah!']);
     }
 }
